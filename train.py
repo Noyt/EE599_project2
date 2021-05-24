Added softmax func in helpers, and corrected train to calculate accuracy when using CrossEntropyLoss

train.py
@@ -1,6 +1,6 @@
 from modules import CrossEntropyLoss
-import torch
-import torch.nn.functional as F
+import helpers
+
 
 def train(model, optimizer, criterion, train_set, train_target, test_set, test_target, epochs=100):
     train_losses, val_losses, accuracies = [],[],[]
@@ -14,25 +14,33 @@ def train(model, optimizer, criterion, train_set, train_target, test_set, test_t
             train_loss += criterion.forward(model_output, target).data.item()
             model.backward(criterion.backward())
             optimizer.step()
+        train_loss = train_loss/len(train_set)
 
         # Validation
         num_correct = 0
-        num_examples = 0
+        num_examples = len(test_target)
         for input, target in zip(test_set, test_target):
             model_output = model.forward(input)
             val_loss += criterion.forward(model_output, target).data.item()
-            correct = torch.eq(torch.max(F.softmax(model_output, dim= 0), dim=1)[1], target).view(-1)
-            num_correct += torch.sum(correct).item()
-            num_examples += correct.shape[0]
+            if type(criterion) == CrossEntropyLoss:
+                found_val = helpers.softmax(model_output).argmax()
+                if found_val == target:
+                    num_correct += 1
+
+        val_loss = val_loss/len(test_set)
 
 
 
-        print(
-            f'Epoch {epoch}, Training Loss: {train_loss:.2f}, Validation Loss : {val_loss:.2f}')#, accuracy = {num_correct / num_examples:.2f}')
-
-        print(f"Accuracy : {num_correct/num_examples}")
-        train_losses.append(train_loss)
-        val_losses.append(val_loss)
-        #accuracies.append(num_correct / num_examples)
+        if type(criterion) == CrossEntropyLoss:
+            print(
+                f'Epoch {epoch}, Training Loss: {train_loss:.2f}, Validation Loss : {val_loss:.2f}, accuracy = {num_correct / num_examples:.2f}')
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+            accuracies.append(num_correct / num_examples)
+        else:
+            print(
+                f'Epoch {epoch}, Training Loss: {train_loss:.2f}, Validation Loss : {val_loss:.2f}')
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
 
     return train_losses, val_losses, accuracies